67bfbeb0995321d92a024e481ea55d1d46614330
Thomas Körner <t.koerner@heimrich-hannot.de>
2018-12-04 14:26:11 +0100

    dynamically load language files

--- src/Resources/assets/js/contaoFlatpickrBundle.es6.js
+++ src/Resources/assets/js/contaoFlatpickrBundle.es6.js
@@ -7,25 +7,47 @@ function FlatpickrBundle(selector) {
     {
         return;
     }
-    elements.forEach((element, key, parent) => {
-        let options = {
-            wrap: true,
-            time_24hr: true
-        };
-        let inputElement = element.querySelector('input[type="text"]');
-        if (inputElement.dataset.enableTime === '1') options.enableTime = true;
-        if (inputElement.dataset.noCalendar === '1') options.noCalendar = true;
-        if (inputElement.dataset.minDate) options.minDate = inputElement.dataset.minDate;
-        if (inputElement.dataset.maxDate) options.maxDate = inputElement.dataset.maxDate;
-        if (inputElement.dataset.dateFormat) options.dateFormat = inputElement.dataset.dateFormat;
-        if (inputElement.dataset.enableAmPm === '1') options.time_24hr = false;
 
-        console.log(options);
-        console.log(inputElement.dataset);
+    const defaultConfig = {
+        wrap: true,
+        time_24hr: true,
+    };
 
-        flatpickr(element, options);
+    this.createFlatpickrInstances = function()
+    {
+        console.log("Default config", defaultConfig);
+        elements.forEach((element, key, parent) => {
+            let options = Object.assign({}, defaultConfig);
+            let inputElement = element.querySelector('input[type="text"]');
+            if (inputElement.dataset.enableTime === '1') options.enableTime = true;
+            if (inputElement.dataset.noCalendar === '1') options.noCalendar = true;
+            if (inputElement.dataset.minDate) options.minDate = inputElement.dataset.minDate;
+            if (inputElement.dataset.maxDate) options.maxDate = inputElement.dataset.maxDate;
+            if (inputElement.dataset.dateFormat) options.dateFormat = inputElement.dataset.dateFormat;
+            if (inputElement.dataset.enableAmPm === '1') options.time_24hr = false;
+
+            // console.log("elements", element);
+            console.log("options", options);
+
+            flatpickr(element, options);
+        });
+    };
+
+
+    let lang = document.querySelector('html').getAttribute('lang');
+
+    import(/* webpackChunkName: "flatpickr-[request]" */ 'flatpickr/dist/l10n/' + lang + '.js').then((locale) =>
+    {
+        flatpickr.localize(locale.default[lang]);
+        this.createFlatpickrInstances();
+
+    }).catch(() => {
+        console.log("error");
+        this.createFlatpickrInstances()
     });
 }
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
     new FlatpickrBundle('.flatpickr');
