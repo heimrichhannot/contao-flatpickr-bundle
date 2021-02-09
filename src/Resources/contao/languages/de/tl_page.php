56f4375f86a10b0ace049de6b17aba3073946ba4
Alexej Kossmann <a.kossmann@heimrich-hannot.de>
2021-02-09 13:57:40 +0100

    added tl_page translation

--- src/Resources/contao/languages/de/tl_page.php
+++ src/Resources/contao/languages/de/tl_page.php
@@ -4,7 +4,3 @@ $lang = &$GLOBALS['TL_LANG']['tl_page'];
 
 $lang['useFlatpickrForDatepicker'][0] = 'Support für Flatpickr hinzufügen';
 $lang['useFlatpickrForDatepicker'][1] = 'Wählen Sie diese Option, wenn dieses Feld als Flatpickr dargestellt werden soll.';
-
-
-$lang['addFlatpickrSupport'][0] = 'Support für Flatpickr hinzufügen';
-$lang['addFlatpickrSupport'][1] = 'Wählen Sie diese Option, wenn dieses Feld als Flatpickr dargestellt werden soll.';
