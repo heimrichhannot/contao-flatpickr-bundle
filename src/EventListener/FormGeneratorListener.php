444a017b3ef7461264c25827cdeb8fd5fcfb5c60
Thomas Körner <t.koerner@heimrich-hannot.de>
2022-02-14 17:17:20 +0100

    flatpickr working

--- src/EventListener/FormGeneratorListener.php
+++ src/EventListener/FormGeneratorListener.php
@@ -5,14 +5,34 @@ namespace HeimrichHannot\FlatpickrBundle\EventListener;
 use Contao\CoreBundle\ServiceAnnotation\Hook;
 use Contao\Form;
 use Contao\Widget;
+use HeimrichHannot\FlatpickrBundle\Util\FlatpickrUtil;
 
 class FormGeneratorListener
 {
+    private FlatpickrUtil $flatpickrUtil;
+
+    public function __construct(FlatpickrUtil $flatpickrUtil)
+    {
+        $this->flatpickrUtil = $flatpickrUtil;
+    }
+
+
     /**
      * @Hook("loadFormField")
      */
     public function onLoadFormField(Widget $widget, string $formId, array $formData, Form $form): Widget
     {
+        if ("text" !== $widget->type || !in_array($widget->rgxp, ['date', 'time', 'datim'])) {
+            return $widget;
+        }
+
+        $attributes = $this->flatpickrUtil->getFlatpickrAttributes([
+            'flatpickr' => ['active' => true],
+            'rgxp' => $widget->rgxp,
+            'type' => $widget->type,
+        ]);
+        $widget->addAttributes($attributes);
+
         return $widget;
     }
 }
